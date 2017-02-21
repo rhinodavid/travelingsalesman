e769b7a39df9754926de5853f25992847f1e96f5
David Walsh <dawalsh@gmail.com>
2017-02-20 19:43:51 -0500

    Compute powerset via bitset and clean

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 
 	"github.com/rhinodavid/bitset"
-	subset "github.com/rhinodavid/travellingsalesman/set"
 )
 
 var (
@@ -25,6 +24,15 @@ type coords struct {
 
 func cartesianDist(a, b *coords) float32 {
 	return float32(math.Sqrt(float64((a.y-b.y)*(a.y-b.y) + (a.x-b.x)*(a.x-b.x))))
+}
+
+func generateCache(subsets []bitset.Bitset, n int) map[bitset.Bitset]map[int]float32 {
+	r := make(map[bitset.Bitset]map[int]float32)
+	for _, sS := range subsets {
+		r[sS] = make(map[int]float32)
+
+	}
+	return r
 }
 
 func main() {
@@ -56,6 +64,7 @@ func main() {
 		a[i] = &coords{x: float32(x), y: float32(y)}
 		i++
 	}
+
 	// compute distance matrix for reuse
 	distances := make([][]float32, n)
 	for i := 0; i < n; i++ {
@@ -67,12 +76,16 @@ func main() {
 	log.Printf("Finsihed generating distances\n")
 
 	// generate all subset combinations
-	subsets := subset.GenerateSubsets(n)
+	indexes := []int{}
+	for i := 1; i < n; i++ {
+		indexes = append(indexes, i)
+	}
+	fullSet := bitset.NewFromSlice(indexes)
+	subsets := fullSet.PowerSet()
 	log.Printf("Finished generating subsets\n")
 
-	//build first cache
-	oldCache := subset.GenerateCache(subset.FilterByCardinality(subsets, 0), n)
-	// initialize first cache
+	// build and initialize first cache
+	oldCache := generateCache(subsets[0], n)
 	for i := 1; i < n; i++ {
 		dist := distances[0][i]
 		for key := range oldCache {
@@ -83,13 +96,10 @@ func main() {
 
 	// iterate over lengths of subsets m
 	for m := 1; m < n-1; m++ {
-		fmt.Printf("\n\nInitiating subsets of size %d\n", m)
-		cardinalSets := subset.FilterByCardinality(subsets, m)
-		newCache := subset.GenerateCache(cardinalSets, n)
-		lenSets := len(cardinalSets)
+		newCache := generateCache(subsets[m], n)
+		fmt.Printf("\rInitiating subsets of size %d", m)
 		i := 1
 		for ss := range newCache {
-			fmt.Printf("\rProcessing subset %d of %d", i, lenSets)
 			i++
 			// iterate over each item in the subset
 			for _, k := range ss.ToSlice() {
@@ -107,7 +117,7 @@ func main() {
 					if !ok {
 						log.Fatalf("Error looking up set %v in cache", sPrime)
 					}
-					sk := cv[j]
+					sk := cv[k]
 					if sk+kj < newCache[ss][j] {
 						newCache[ss][j] = sk + kj
 					}
@@ -116,15 +126,18 @@ func main() {
 		}
 		oldCache = newCache
 	}
+
 	// compute final subset
+	fmt.Println()
+	log.Printf("Computing final result from last cache\n")
 	intSlice := make([]int, n-1)
 	for i := 0; i < n-1; i++ {
 		intSlice[i] = i + 1
 	}
 	finalSS := bitset.NewFromSlice(intSlice)
+
 	// compute result from last cache
 	result := maxFloat
-
 	for _, k := range finalSS.ToSlice() {
 		kj := distances[k][0]
 		sPrime := finalSS.RemoveMember(k)
